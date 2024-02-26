32f13de8a928e734a99ab3d835dc8f34a93f15e3
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-02-26 06:57:00 +0000

    #0: Fix dimension 3

--- tests/tt_eager/python_api_testing/sweep_tests/common.py
+++ tests/tt_eager/python_api_testing/sweep_tests/common.py
@@ -51,12 +51,7 @@ def run_tt_lib_test(
     pytorch_out = pytorch_op(*tensor_inputs, **test_args)
 
     result, output = output_comparison_func(pytorch_out, tt_lib_out)
-    torch.set_printoptions(linewidth=200, precision=5, threshold=10000, edgeitems=17)
 
-    print("TT result = ", tt_lib_out)
-    print("output = ", pytorch_out)
-    print("TT result shape      :", tt_lib_out.shape)
-    print("Pytorch Output shape :", pytorch_out.shape)
     if plot_func is not None:
         test_name = str(pytorch_op).split()[1]
         plot_func(test_name, *tensor_inputs, pytorch_out, tt_lib_out)
